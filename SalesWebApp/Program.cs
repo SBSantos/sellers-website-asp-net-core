229bd86644727c55e40fb8bdb6826bd34191e2d8
Funkenstein <Saymon.801.sb@gmail.com>
2025-07-23 21:38:36 -0300

    SellersController, SellerService and FindAll

--- SalesWebApp/Program.cs
+++ SalesWebApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebApp.Data;
+using SalesWebApp.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 ServerVersion serverVersion = ServerVersion.Create(
@@ -15,6 +16,7 @@ builder.Services.AddDbContext<SalesWebAppContext>(options => options.UseMySql(
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 var app = builder.Build();
 
---
+++ SalesWebApp/Services/SellerService.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebApp.Data;
+using SalesWebApp.Models;
+
+namespace SalesWebApp.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebAppContext _context;
+
+        public SellerService(SalesWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
