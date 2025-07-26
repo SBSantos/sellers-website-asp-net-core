3c988a351eaacf5494635d3306f3a641ad438b40
Funkenstein <Saymon.801.sb@gmail.com>
2025-07-26 00:09:10 -0300

    Sellers form

--- SalesWebApp/Controllers/SellersController.cs
+++ SalesWebApp/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebApp.Services;
+using SalesWebApp.Models;
 
 namespace SalesWebApp.Controllers
 {
@@ -16,6 +17,19 @@ namespace SalesWebApp.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction("Index");
         }
     }
 }
