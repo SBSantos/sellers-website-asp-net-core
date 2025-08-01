85fede3d97946bee175751c08079a2e6620c69c5
Funkenstein <Saymon.801.sb@gmail.com>
2025-07-31 23:03:22 -0300

    Seller details and eager loading

--- SalesWebApp/Services/SellerService.cs
+++ SalesWebApp/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebApp.Data;
 using SalesWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebApp.Services
 {
@@ -23,9 +24,9 @@ namespace SalesWebApp.Services
             _context.SaveChanges();
         }
 
-        public Seller FindById(int id)
+        public Seller? FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
